bded9d5e0fc29e715239d90de7036f692f3608e3
Anders Petersen <awp@eamv.dk>
2015-02-02 10:12:37 +0100

    Test af klasselokale på 40 m2 implementeret.

--- Opgave 5/src/Test.java
+++ Opgave 5/src/Test.java
@@ -1,11 +1,24 @@
+import lokaler.KlasseLokale;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		testKlasseLokalePå40Kvm();
+//		testKlasseLokalePå30Kvm();
+//		testKopiAfKlasseLokalePå40Kvm();
+//		testEDB_LokalePå20KvmMed25EnhederStrøm();
+//		testEDB_LokalePå60KvmMed70EnhederStrøm();
 	}
 	
 	private static void testKlasseLokalePå40Kvm() {
-		
+		System.out.println("Forventet output:");
+		System.out.println("  Lokaletype: Klasselokale");
+		System.out.println("  Areal:      40 m2");
+		System.out.println("  Strømudtag: 0 enheder");
+		System.out.println("  Kapacitet:  16 studerende");
+		System.out.println("Faktisk output:");
+		System.out.println(new KlasseLokale(40, 0));
+		System.out.println();
 	}
 	
 	private static void testKlasseLokalePå30Kvm() {
