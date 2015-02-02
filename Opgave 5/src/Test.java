26d176f73a0f3a2e51470c877fb726cc3b85af13
Anders Petersen <awp@eamv.dk>
2015-02-02 10:15:15 +0100

    Test af kopi af klasselokale på 40 m2 implementeret.

--- Opgave 5/src/Test.java
+++ Opgave 5/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		testKlasseLokalePå40Kvm();
 		testKlasseLokalePå30Kvm();
-//		testKopiAfKlasseLokalePå40Kvm();
+		testKopiAfKlasseLokalePå40Kvm();
 //		testEDB_LokalePå20KvmMed25EnhederStrøm();
 //		testEDB_LokalePå60KvmMed70EnhederStrøm();
 	}
@@ -33,7 +33,14 @@ public class Test {
 	}
 	
 	private static void testKopiAfKlasseLokalePå40Kvm() {
-		
+		System.out.println("Forventet output:");
+		System.out.println("  Lokaletype: Klasselokale");
+		System.out.println("  Areal:      40 m2");
+		System.out.println("  Strømudtag: 0 enheder");
+		System.out.println("  Kapacitet:  16 studerende");
+		System.out.println("Faktisk output:");
+		System.out.println(new KlasseLokale(new KlasseLokale(40, 0)));
+		System.out.println();
 	}
 	
 	private static void testEDB_LokalePå20KvmMed25EnhederStrøm() {
