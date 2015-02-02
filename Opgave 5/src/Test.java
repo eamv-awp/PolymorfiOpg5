7720117565d580e0b9289da8ea026f00b9170904
Anders Petersen <awp@eamv.dk>
2015-02-02 10:06:19 +0100

    Metodestubbe til test er oprettet i Test-klassen.

--- Opgave 5/src/Test.java
+++ Opgave 5/src/Test.java
@@ -3,5 +3,25 @@ public class Test {
 	public static void main(String[] args) {
 		
 	}
+	
+	private static void testKlasseLokalePå40Kvm() {
+		
+	}
+	
+	private static void testKlasseLokalePå30Kvm() {
+		
+	}
+	
+	private static void testKopiAfKlasseLokalePå40Kvm() {
+		
+	}
+	
+	private static void testEDB_LokalePå20KvmMed25EnhederStrøm() {
+		
+	}
+	
+	private static void testEDB_LokalePå60KvmMed70EnhederStrøm() {
+		
+	}
 
 }
