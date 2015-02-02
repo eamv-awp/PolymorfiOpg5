a13dca2c6c691444aa40f0bc6e6a90492c85a973
Anders Petersen <awp@eamv.dk>
2015-02-02 10:17:49 +0100

    Test af EDB-lokale på 20 m2 og strømudtag på 25 enheder implementeret.

--- Opgave 5/src/Test.java
+++ Opgave 5/src/Test.java
@@ -1,3 +1,4 @@
+import lokaler.EDB_Lokale;
 import lokaler.KlasseLokale;
 
 public class Test {
@@ -6,7 +7,7 @@ public class Test {
 		testKlasseLokalePå40Kvm();
 		testKlasseLokalePå30Kvm();
 		testKopiAfKlasseLokalePå40Kvm();
-//		testEDB_LokalePå20KvmMed25EnhederStrøm();
+		testEDB_LokalePå20KvmMed25EnhederStrøm();
 //		testEDB_LokalePå60KvmMed70EnhederStrøm();
 	}
 	
@@ -14,7 +15,7 @@ public class Test {
 		System.out.println("Forventet output:");
 		System.out.println("  Lokaletype: Klasselokale");
 		System.out.println("  Areal:      40 m2");
-		System.out.println("  Strømudtag: 0 enheder");
+		System.out.println("  Strømudtag:  0 enheder");
 		System.out.println("  Kapacitet:  16 studerende");
 		System.out.println("Faktisk output:");
 		System.out.println(new KlasseLokale(40, 0));
@@ -25,7 +26,7 @@ public class Test {
 		System.out.println("Forventet output:");
 		System.out.println("  Lokaletype: Klasselokale");
 		System.out.println("  Areal:      30 m2");
-		System.out.println("  Strømudtag: 0 enheder");
+		System.out.println("  Strømudtag:  0 enheder");
 		System.out.println("  Kapacitet:  12 studerende");
 		System.out.println("Faktisk output:");
 		System.out.println(new KlasseLokale(30, 0));
@@ -36,7 +37,7 @@ public class Test {
 		System.out.println("Forventet output:");
 		System.out.println("  Lokaletype: Klasselokale");
 		System.out.println("  Areal:      40 m2");
-		System.out.println("  Strømudtag: 0 enheder");
+		System.out.println("  Strømudtag:  0 enheder");
 		System.out.println("  Kapacitet:  16 studerende");
 		System.out.println("Faktisk output:");
 		System.out.println(new KlasseLokale(new KlasseLokale(40, 0)));
@@ -44,7 +45,14 @@ public class Test {
 	}
 	
 	private static void testEDB_LokalePå20KvmMed25EnhederStrøm() {
-		
+		System.out.println("Forventet output:");
+		System.out.println("  Lokaletype: EDB-lokale");
+		System.out.println("  Areal:      20 m2");
+		System.out.println("  Strømudtag: 25 enheder");
+		System.out.println("  Kapacitet:   8 studerende");
+		System.out.println("Faktisk output:");
+		System.out.println(new EDB_Lokale(20, 25));
+		System.out.println();
 	}
 	
 	private static void testEDB_LokalePå60KvmMed70EnhederStrøm() {
