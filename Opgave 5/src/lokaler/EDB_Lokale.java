96172552c2567ba63708b32acfb9cbe9f4f10cf2
Anders Petersen <awp@eamv.dk>
2015-02-02 09:32:26 +0100

    Kommentar tilføjet vedrørende nedarvingshierarkiet.

--- Opgave 5/src/lokaler/EDB_Lokale.java
+++ Opgave 5/src/lokaler/EDB_Lokale.java
@@ -1,5 +1,16 @@
 package lokaler;
 
+/**
+ * Man kan argumentere for at EDB_Lokale burde nedarve fra {@link KlasseLokale} 
+ * fordi et EDB-lokale netop er et specialiseret klasselokale. 
+ * Nedarving fra KlasseLokale vil tillade at EDB_Lokale kan 
+ * genbruge resultatet af beregningen af kapacitet fra KlasseLokale.
+ * 
+ * @see KlasseLokale#kapacitet()
+ * @see EDB_Lokale#kapacitet()
+ * 
+ * @author awp
+ */
 public class EDB_Lokale extends Lokale {
 
 	public EDB_Lokale(int areal, int strøm) {
